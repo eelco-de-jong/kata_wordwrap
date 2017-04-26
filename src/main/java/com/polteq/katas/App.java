e961b547c00e746b28255cd2ce8cc1aef4234769
Eelco de Jong <jong.de.eelco@gmail.com>
2017-04-26 16:25:23 +0200

    Added todo items

---
+++ src/main/java/com/polteq/katas/App.java
@@ -0,0 +1,13 @@
+package com.polteq.katas;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/polteq/katas/WordWrap_should.java
@@ -0,0 +1,15 @@
+package com.polteq.katas;
+
+/**
+ * Created by jongd on 26-4-2017.
+ *
+ * TODO:
+ * 1. Assert that a word is returned correct | ("tester", 7) >> "tester"
+ * 2. Assert that a newline is added correct | ("tester", 3) >> "tes\nter"
+ * 3. Assert that a newline is added instead of a space | ("testers test", 9) >> "testers\ntest"
+ * 4. Assert that a newline is added on space | ("testers test", 11) >> "testers\ntest"
+ * 5. Assert that a mulitple wraps work correct | ("testers test testthingies", 8) >> "testers\ntest\ntestthi\nngies"
+ */
+
+public class WordWrap_should {
+}
